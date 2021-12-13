2de39fbe5c57e9ed8f36a2303deb4ab0ed81d59b
shubham <devshubhamyadav@gmail.com>
2021-12-14 00:33:16 +0530

    Add project files.

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("CatchMeBaby")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("CatchMeBaby")>
+<Assembly: AssemblyCopyright("Copyright ©  2021")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("7d72aca0-f63a-4034-a2af-7b37d38f2b7d")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ catchMeBaby.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim picNum = 1
+        Dim xObj = New Random()
+        Dim yObj = New Random()
+        Dim picObj = New Random().Next(1, 6)
+        Dim x = xObj.Next(Me.Width - btnCatchMe.Width)
+        Dim y = yObj.Next(Me.Height - btnCatchMe.Height)
+        lblCord.Text = "I am at x = " & x & " and y = " & y
+        pcbEmoji.Image = Image.FromFile(picObj & ".png")
+        btnCatchMe.Location = New Point(x, y)
+    End Sub
+End Class
